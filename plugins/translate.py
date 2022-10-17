eaed440e7ee0e13e82fa2bcccab4a6325fd6d46b
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-10-17 11:35:15 +0600

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -69,7 +69,7 @@ async def echo(client, message):
 			except Exception as e:
 					await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```\n\n join @lntechnical")
 	else:
-		await  message.reply_text("Select language 👇",reply_to_message_id = message.message_id, reply_markup =keybord1)
+		await  message.reply_text("Select language 👇",reply_to_message_id = message.id, reply_markup =keybord1)
 
 @Client.on_callback_query()
 async def translate_text(bot,update):
